17c2875c576f18a59860fe122410b90664100a71
pranavkuruvella <pranavkuruvella@gmail.com>
2026-03-03 17:10:53 +0530

    implemented home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,57 @@
+import TradingViewWidget from '@/components/TradingViewWidget'
 import { Button } from '@/components/ui/button'
+import { HEATMAP_WIDGET_CONFIG, MARKET_DATA_WIDGET_CONFIG, MARKET_OVERVIEW_WIDGET_CONFIG, TOP_STORIES_WIDGET_CONFIG } from '@/lib/constants'
 import React from 'react'
 
 //root -- home page folder
 const page = () => {
+  const scriptUrl = 'https://s3.tradingview.com/external-embedding/embed-widget-'
   return (
     <div className='flex min-h-screen home-wrapper'>
-      home
+      <section className='grid w-full gap-8 home-section'>
+        {/* market overview */}
+        <div className='md:col-span-1 xl:col-span-1'>
+          <TradingViewWidget
+            title='Market Overview'
+            scriptUrl={scriptUrl + 'market-overview.js'}
+            config={MARKET_OVERVIEW_WIDGET_CONFIG}
+            height={600}
+            className='' />
+        </div>
+
+        {/* stock heatmap */}
+        <div className='md:col-span xl:col-span-2'>
+          <TradingViewWidget
+            title='Stock Heatmap'
+            scriptUrl={scriptUrl + 'stock-heatmap.js'}
+            config={HEATMAP_WIDGET_CONFIG}
+            height={600}
+            className='' />
+        </div>
+      </section>
+
+
+      <section className='grid w-full gap-8 home-section'>
+        {/* top stories */}
+        <div className=' h-full md:col-span-1 xl:col-span-1'>
+          <TradingViewWidget
+            title=''
+            scriptUrl={scriptUrl + 'timeline.js'}
+            config={TOP_STORIES_WIDGET_CONFIG}
+            height={600}
+            className='custom-chart' />
+        </div>
+
+        {/* market quotes */}
+        <div className='h-full md:col-span-1 xl:col-span-2'>
+          <TradingViewWidget
+            title=''
+            scriptUrl={scriptUrl + 'market-quotes.js'}
+            config={MARKET_DATA_WIDGET_CONFIG}
+            height={600}
+            className='' />
+        </div>
+      </section>
     </div>
   )
 }
---
+++ components/TradingViewWidget.tsx
@@ -0,0 +1,33 @@
+// TradingViewWidget.jsx
+'use client'
+import React, { useRef, memo } from 'react';
+import useTradingViewWidget from './hooks/useTradingViewWidget';
+import { cn } from '@/lib/utils';
+
+//this is a component that will be used to display the tradingview widget -- taken from tradingview docs
+interface TradingViewWidgetProps {
+  title: string;
+  scriptUrl: string;
+  config: Record<string, unknown>;
+  height?: number;
+  className?: string;
+}
+
+function TradingViewWidget({ title, scriptUrl, config, height = 600, className }: TradingViewWidgetProps) {
+  const containerRef = useTradingViewWidget(scriptUrl, config, height);
+
+
+  return (
+    <div className='w-full'>
+      {title && <h3 className='font-bold text-2xl text-gray-100 mb-5'>{title}</h3>}
+
+      {/* the chart container */}
+
+      <div className="tradingview-widget-container" ref={containerRef} style={{ height: "100%", width: "100%" }}>
+        <div className={cn("tradingview-widget-container__widget", className)} style={{ height: height, width: "100%" }} />
+      </div>
+    </div>
+  );
+}
+
+export default memo(TradingViewWidget);
