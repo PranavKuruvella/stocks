d84d55cd2b5be45d28474b5c4e268285214b5b7a
pranavkuruvella <pranavkuruvella@gmail.com>
2026-03-04 17:48:54 +0530

    implemented db setup

---
+++ database/mongoose.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+
+declare global {
+  var mongooseCache: {
+    conn: typeof mongoose | null
+    promise: Promise<typeof mongoose> | null
+  }
+}
+
+let cached = global.mongooseCache;
+
+if (!cached) {
+  cached = global.mongooseCache = { conn: null, promise: null }
+}
+
+export const connectDB = async () => {
+
+  if (!MONGODB_URI) {
+    throw new Error("Please add your Mongo URI to .env.local");
+  }
+  if (cached.conn) return cached.conn;
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+  console.log("DB connected");
+}
