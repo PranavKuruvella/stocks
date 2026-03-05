b1ec419941e738457f6cc2620048ea9238fc6679
pranavkuruvella <pranavkuruvella@gmail.com>
2026-03-05 20:49:15 +0530

    implemented middleware

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
-//import {auth} from "@/lib/better-auth/auth";
+import {auth} from "@/lib/better-auth/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
-  //const session = await auth.api.getSession({ headers: await headers() })
+  //middleware - check if user is already logged in
+  const session = await auth.api.getSession({ headers: await headers() })
 
-  //if(session?.user) redirect('/')
+  if(session?.user) redirect('/')
 
   return (
     <main className="auth-layout">
